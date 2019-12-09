cebcca30abe89aacf9d9b2173d814035a9d2ac26
brian <brian@pindad.com>
2019-12-09 15:42:00 +0700

    second commit

--- 02.js
+++ 02.js
@@ -1,14 +1,8 @@
-function palindrome(str) {
-  var lowercase = str.toLowerCase();
-  var stringreverse = lowercase
-    .split(' ')
-    .reverse()
-    .join(' ');
-  return stringreverse;
-}
-const string1 = "ibu ratna antar ubi";
-const string2 = "kasur ini rusak";
-const result1 = palindrome(string1).split(' ');
-const result2 = palindrome(string2).split(' ');
-console.log(result1);
-console.log(result2);
+const first = ['Behind', 'every', 'great', 'man']
+      second = ['is', 'a', 'woman']
+      third = ['rolling', 'her', 'eyes']
+
+let gabungArray = first.concat(second, third)
+let kalimat = gabungArray.join(' ');
+
+console.log(kalimat)
